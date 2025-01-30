3e8f292cdd76396396c2328a1a455947e2c3a171
laeth0 <laethraed0@gmail.com>
2025-01-30 11:34:44 +0200

    second

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import "bootstrap/dist/JS/bootstrap.bundle.js";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 
 
 import './index.css'
